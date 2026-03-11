8b60d8e296a57ed1167b5de3563190835f3a680e
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2026-03-11 15:23:04 -0400

    Add DuckDuckGo library to make search_web tooling function actually explore things on internet

--- research_agent.py
+++ research_agent.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+from ddgs import DDGS
 
 load_dotenv()
 
@@ -9,15 +10,25 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 # tool calling
 def search_web(query: str) -> dict:
     """Search the web for the information on a topic."""
-    return{}
+    ddgs = DDGS()
+    results = ddgs.text(query, max_results=5)
+    
+    output = ""
+    for r in results:
+        output += f"Title: {r['title']}\n"
+        output += f"Snippet: {r['body']}\n\n"
+    
+    return output
+print(search_web("latest trends in edge AI"))
+
 
 model = genai.GenerativeModel(
     model_name="gemini-2.5-flash",
     tools=[search_web],
     system_instruction="You are a research agent. When asked about current events or recent information, ALWAYS use the search_web tool. Never answer from memory."
 )
-
 response = model.generate_content("Search for the latest trends in edge AI")
-
-
 print(response.candidates[0].content.parts)
+
+
+    
